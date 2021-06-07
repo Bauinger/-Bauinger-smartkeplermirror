71b6fc642b5dc2ffbc6638f446fd6fbfb7bf21ce
Bauinger <bernhard@auinger.cc>
2021-06-07 14:47:25 +0200

    Updated Quotes.vue to Database

--- frontend/smart-kepler/src/Quotes.vue
+++ frontend/smart-kepler/src/Quotes.vue
@@ -15,6 +15,8 @@
 <script>
 import { Hooper, Slide } from 'hooper';
 import 'hooper/dist/hooper.css';
+const Datastore = require('nedb-promises')
+let db = Datastore.create('./db/quotes.db')
 export default {
   name: 'Quotes',
   components: {
@@ -23,28 +25,58 @@ export default {
   },
   data: function() {
     return {
-      quotes : null,
+      quotes : [],
       errored: false,
       timer : '',
     };
   },
-  mounted() {
-    this.loadQuotes();
-    this.timer = setInterval(this.loadQuotes(), 3600000)
+  async mounted() {
+    await this.setupDatabase();
+    await this.loadQuotes();
+    this.timer = setInterval(await this.loadQuotes(), 3600000)
   },
-  beforeDestroy (){
+  beforeDestroy () {
     this.cancelAutoUpdate();
   },
-  methods: {
+  methods: {  
     async loadQuotes() {
-      let apiUrl = 'http://localhost:8081/quote';
-      let response = await this.axios.get(apiUrl)
-                                     .catch(error => {
-                                         console.log(error)
-                                         this.errored = true;
-                                       });
-      this.quotes = response.data;
-      console.log(response.data);
+      let doc = await db.find({})
+                        .then(docs => {
+                          return docs;
+                        })
+                        .catch(err => {console.log(err)});
+      console.log(doc);
+      this.quotes = doc;
+    },
+    async setupDatabase() {
+      await db.remove({}, {multi: true})
+        .catch( err => {
+          console.log(err)
+        });
+      await db.insert({quoteText : '„Nur die Liebe zur Wahrheit schafft Wunder.“'});
+      await db.insert({quoteText : '„Die Sterne sind nur der Vater deines Schicksals. Die Mutter ist deine eigene Seele.“'},
+                  {quoteText : '„Hundert kleine Freuden sind tausendmal mehr wert als eine große.“'},
+                  {quoteText : '„Die Welt ist ein körperliches Bild Gottes, der Geist ist ein unkörperliches, aber doch erschaffenes Bild Gottes.“'},
+                  {quoteText : '„Mein höchster Wunsch ist, den Gott, den ich im Äußerem überall finde, auch innerlich, innerhalb meiner gleichermaßen gewahr zu werden.“'},
+                  {quoteText : '„Es gibt nichts Wunderbareres, - nichts, was die Weisheit des Schöpfers bei den Verständigen heller bezeugt, als die Bewegung der fünf Planeten.“'},
+                  {quoteText : '„Wenn es gar so dunkel ist in deinem Leben, sieh doch einmal nach, ob es nicht am Ende daher kommt, daß alle deine Fensterläden verschlossen sind.“'},
+                  {quoteText : '„Das Beste findet sich dort, wo sich Fleiß mit Begabung verbindet.“'},
+                  {quoteText : '„Die Geometrie gab es schon vor der Erschaffung der Welt. Sie ist ewig wie der Geist Gottes.“'},
+                  {quoteText : '„Die Heilige Schrift, welche höhere Dinge lehrt, bedient sich der gewöhnlichen Redeweise, um verstanden zu werden.“'},
+                  {quoteText : '„Täglich steht der Tod mir bevor, wohl weiß ich es sicher. Doch wenn ich schau, wie der Chor kreisender Sterne sich schlingt, fühl ich mich aufwärts gehoben, ich sitze an himmlischer Tafel.“'},
+                  {quoteText : '„Ich wollte Theologe werden; lange war ich in Unruhe. Nun aber sehet, wie Gott durch mein Bemühen auch in der Astronomie gefeiert wird.“'},
+                  {quoteText : '„Die Körper wären nicht schön, wenn sie sich nicht bewegten.“'},
+                  {quoteText : '„Die Mathematik allein befriedigt den Geist durch ihre außerordentliche Gewissheit.“'},
+                  {quoteText : '„Alles, was im Himmel ist oder geschieht, macht sich auf irgendeine verborgene Weise auf Erden und in der Natur bemerkbar.“'},
+                  {quoteText : '„Ein Mangel an Phantasie bedeutet den Tod der Wissenschaft.“'},
+                  {quoteText : '„Mir kommen die Wege, auf denen die Menschen zur Erkenntnis gelangen fast ebenso bewunderungswürdig vor wie die Natur der Dinge selbst.“'},
+                  {quoteText : '„O wie der Mensch sich quält, o wie eitel ist das Treiben der Welt.“'},
+                  {quoteText : '„Die Sterne können zwar die Seele eines Menschen erfüllen, den Erfolg selber aber vermögen sie nicht zu gewährleisten. Sie rütteln vielmehr den Menschen auf und stellen ihn gewissermaßen auf Wachposten, damit er die vorüberziehenden Gelegenheiten ergreift.“'},
+        )
+        .catch( err => {
+          console.log(err)
+        });
+        console.log('Initialized Database!')
     },
     cancelAutoUpdate () {
       clearInterval(this.timer);
